324eccef7ee2fc8665da2e222322f6764b6c9b3c
Sebastian Czech <sebaczech@gmail.com>
2022-09-11 20:59:58 +0200

    Change name of table

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "aws_dynamodb_table" "table_create_via_github_actions" {
+resource "aws_dynamodb_table" "table_create_via_gitlab_pipeline" {
   provider       = aws.cloud
-  name           = "table_create_via_github_actions"
+  name           = "table_create_via_gitlab_pipeline"
   read_capacity  = "20"
   write_capacity = "20"
   hash_key       = "ID"
